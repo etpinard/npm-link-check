0f788a86a34763f46eac69bacc7a40e3045b5573
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2016-03-15 18:11:24 -0400

    throw error on process exit:
    - instead of logging on found packages
    - rm chalk from deps

--- cmd.js
+++ cmd.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
 var path = require('path');
-var chalk = require('chalk');
 var npmLinkCheck = require('./index');
 
 var pathToRoot = process.argv[2] ? process.argv[2] : '.';
+var list = [];
 
 
-function cb(pkgName) {
-    process.stderr.write([
-        chalk.bold.red('ERROR:'), pkgName, 'is linked!\n'
+function cb(pkgName, foundPath) {
+    list.push([
+        '  ',
+        '- package', pkgName,
+        '(at ' + foundPath + ')',
+        'is linked'
     ].join(' '));
 }
 
+function log() {
+    var header = 'Some npm-link\'ed packaged were found:'
+
+    throw new Error(header + '\n' + list.join('\n') + '\n');
+}
+
 npmLinkCheck(pathToRoot, cb);
+
+process.on('exit', function() {
+    if(list.length) log();
+})
